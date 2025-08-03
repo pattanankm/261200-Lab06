72bea029e040e5c37d11c8c2b8d75f41bf0de367
pattanankm <khaimookbb212@gmail.com>
2025-08-04 01:06:00 +0700

    add Dog.java

---
+++ Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+    @Override
+    protected void sound(){
+        System.out.println("Hong Woof Woke Wan");
+    }
+
+    public void playFetch(){
+        System.out.println("Throw something");
+    }
+}
