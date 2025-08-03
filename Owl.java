cdb2a61a8f08e3fd3680b6391d974e725629f7b2
pattanankm <khaimookbb212@gmail.com>
2025-08-04 01:07:31 +0700

    Create Owl.java

---
+++ Owl.java
@@ -0,0 +1,15 @@
+public class Owl extends Animal {
+    @Override
+    protected void sound(){
+        System.out.println("hook hoo hu hoo");
+    }
+    public void rotateHeadOver300degree(){
+        System.out.println("Ouch!");
+    }
+    public void fly(){
+        System.out.println("______");
+    }
+    public void glides(){
+        System.out.println("______");
+    }
+}
